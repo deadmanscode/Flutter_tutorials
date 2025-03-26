85e5f8c9cf6b3f6ed4a7991ef0a389862b33b013
moganraaj <moganraaj@gmail.com>
2025-03-26 22:24:57 +0530

    App Bar and Scaffold

--- dart_first_demo/lib/main.dart
+++ dart_first_demo/lib/main.dart
@@ -3,27 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home:(
-        Text("First Flutter Program",style: TextStyle(color: Colors.red),)
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text("TalkTrackRide"),
+          backgroundColor: Colors.deepOrange,
+        ),
+        
         )
       )
     );
 }
-/*
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}*/
