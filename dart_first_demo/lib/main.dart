165b4477ef31a258479370743b8d144bca3d1ceb
moganraaj <moganraaj@gmail.com>
2025-03-26 23:13:50 +0530

    Demo container with in Scaffold body
    Demonstration of Container within the Scaffold body.
    Recommended application structure
    
    Mohanraj

--- dart_first_demo/lib/main.dart
+++ dart_first_demo/lib/main.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home:Scaffold(
+runApp(const MainApp());
+ }
+ 
+ class MainApp extends StatelessWidget {
+   const MainApp({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home: Scaffold(
         appBar: AppBar(
           title: Text("TalkTrackRide"),
           backgroundColor: Colors.deepOrange,
+          centerTitle: true,
         ),
-        
-        )
-      )
-    );
-}
+         body: Center(
+          child:Container(
+            color: Colors.deepOrange,
+            height: 200,
+            width: 400,
+            alignment: Alignment.center,
+            child: Text('App body goes here!!!'),
+
+          ),
+         ),
+       ),
+     );
+   }
+ }
