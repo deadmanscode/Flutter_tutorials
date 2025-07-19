b0aa266dd0f5a76e0b3f3e6a20e7d0bec6e1f52b
moganraaj <moganraaj@gmail.com>
2025-07-19 19:05:23 +0530

    List view builder, texteditcontroller and expand tutorial

--- stateful-widget/stateful_widget_demo/lib/main.dart
+++ stateful-widget/stateful_widget_demo/lib/main.dart
@@ -1,30 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SecondApp());
+  runApp(MainApp());
 }
 
-class SecondApp extends StatefulWidget {
-  const SecondApp({super.key});
-
+class MainApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return SecondAppState();
-  }
+  State<MainApp> createState() => _MainAppState();
 }
 
-class SecondAppState extends State<SecondApp> {
-  List<String> mylist = ["one", "two", "three", "four", "five"];
+class _MainAppState extends State<MainApp> {
+  String displaytext = "text";
+  List<String> tasklist = ["task 1", "task 2"];
+  TextEditingController txtcntlr = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("List view builder")),
-        body: ListView.builder(
-          itemCount: mylist.length,
-          itemBuilder: (context, index) {
-            return Text(mylist[index]);
-          },
+        appBar: AppBar(title: Text("Todo list"), backgroundColor: Colors.blue),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      controller: txtcntlr,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Enter Input"),
+                      ),
+                    ),
+                  ),
+                ),
+
+                MaterialButton(
+                  color: Colors.white,
+                  height: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      displaytext = txtcntlr.text;
+                      tasklist.add(txtcntlr.text);
+                      txtcntlr.clear();
+                    });
+                  },
+                  child: Text("ADD"),
+                ),
+              ],
+            ),
+
+            Flexible(
+              child: ListView.builder(
+                itemCount: tasklist.length,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(tasklist[index]),
+                        ),
+                      ),
+                      MaterialButton(
+                        child: Icon(Icons.delete, color: Colors.deepOrange),
+                        onPressed: () {
+                          setState(() {
+                            tasklist.removeAt(index);
+                          });
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
